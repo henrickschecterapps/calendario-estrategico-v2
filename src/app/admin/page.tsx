a283b45d125c93c2bc304daba10590887102ffd0
Henrique Matos <hmatos@tripla.com.br>
2026-05-12 14:33:58 -0300

    fix(ui): add min-w-0 to prevent flex item from growing past viewport width

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1159,7 +1159,7 @@ export default function AdminDashboard() {
                 </div>
 
                 {/* Right Module Content Area */}
-                <div className="flex-1 w-full flex flex-col gap-6">
+                <div className="flex-1 w-full min-w-0 flex flex-col gap-6">
 
                    {/* Visão Geral Module */}
                    {opTab === 'Visão Geral' && (
